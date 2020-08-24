1546f4e805e1897c61bcc0de485c6dd57ff049b6
Mike Pankin <mishapankin@gmail.com>
2020-08-24 19:03:10 +0300

    Fixed top

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ function resizePage(event) {
     let about = document.getElementById("about_us");
     let text = document.getElementById("text");
 
-    about.style.height = 123 + text.offsetHeight + "px";
+    let x = 123.0 / 1080;
+
+    about.style.height = text.offsetHeight / (1 - x) + "px";
+    text.style.top = x * text.offsetHeight + "px";
 }
 
 window.onload = resizePage;
