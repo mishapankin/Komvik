f86a10ba788f611e9ee38407fee076f3d94a4110
Mike Pankin <mishapankin@gmail.com>
2020-08-26 22:05:01 +0300

    Loads faster

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 'use strict';
 
 function resizePage(event) {
+    resizeAbout();
+    resizeCarousel();
+}
+
+function resizeAbout() {
     let about = document.getElementById("about_us");
     let text = document.getElementById("text");
 
+    let x = 123.0 / 1080;
+
+    about.style.height = text.offsetHeight / (1 - x) + "px";
+    text.style.top = Math.max(x * text.offsetHeight, 123) + "px";
+}
+
+function resizeCarousel() {
     let items = document.getElementsByClassName("service-item");
     let height = 0;
     for (let item of items) {
@@ -15,20 +27,11 @@ function resizePage(event) {
     for (let item of items) {
         item.style.height = height + "px";
     }
-
-    let x = 123.0 / 1080;
-
-    about.style.height = text.offsetHeight / (1 - x) + "px";
-    text.style.top = Math.max(x * text.offsetHeight, 123) + "px";
 }
 
 function moveCarousel(page) {
     $("#servicesCarousel").carousel(page);
 }
 
-window.onload = resizePage;
+window.onload = resizeCarousel;
 window.onresize = resizePage;
-
-$("#servicesCarousel").carousel({
-    interval: false
-})
